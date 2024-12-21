170ff9c73637fd0c4261bbdf8c1ebca5d57b439a
Muhammet Kerem Öztürk <ichbinichbin285@gmail.com>
2024-12-22 00:28:29 +0300

    added basic caching mechanism with redis

---
+++ infra/redis_connector.py
@@ -0,0 +1,24 @@
+import redis
+import os
+
+class RedisConnector:
+    def __init__(self, host='127.0.0.1', port=6379, db=0):
+        self.host = host
+        self.port = port
+        self.db = db
+        self.client = None
+
+    def connect(self):
+        if not self.client:
+            self.client = redis.Redis(
+                host=self.host,
+                port=self.port,
+                db=self.db
+            )
+        return self.client
+
+    def set_item(self, key, value):
+        self.client.set(key, value)
+
+    def get_item(self, key):
+        return self.client.get(key)
